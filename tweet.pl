dc1edf003ce20736ef1f956c5145722be4854e86
Kang-min Liu <gugod@gugod.org>
2021-02-05 09:50:56 +0900

    secret in env

--- tweet.pl
+++ tweet.pl
@@ -18,8 +18,8 @@ GetOptions(
 my $config;
 if ($opts{c} && -f $opts{c}) {
     $config = YAML::LoadFile( $opts{c} );
-} elsif ($opts{'github-secret'} && $ENV{'GITHUB_TWITTER_TOKENS'}) {
-    $config = YAML::Load($ENV{'GITHUB_TWITTER_TOKENS'});
+} elsif ($opts{'github-secret'} && $ENV{'TWITTER_TOKENS'}) {
+    $config = YAML::Load($ENV{'TWITTER_TOKENS'});
 }
 
 # 2021/02/06: https://www.cec.gov.tw/central/cms/110news/34965
