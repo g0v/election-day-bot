84ef4ccc200524369e828855b31e98653c3ba390
Kang-min Liu <gugod@gugod.org>
2021-02-06 11:13:05 +0900

    say something a bit special before start the next countdown

--- tweet.pl
+++ tweet.pl
@@ -1,94 +1,133 @@
 use v5.14;
+use feature 'signatures';
 use utf8;
+
 use Net::Twitter;
 use YAML;
-use Encode qw(encode_utf8);
-use Getopt::Long qw(GetOptions);
 use DateTime;
+use Encode ('encode_utf8');
+use Getopt::Long ('GetOptionsFromArray');
 
-my %opts;
-GetOptions(
-    \%opts,
-    'fake-today=s',
-    'github-secret',
-    'c=s',
-    'y|yes'
-) or die("Error in arguments, but I'm not telling you what it is.");
+sub main {
+    my @args = @_;
 
-my $config;
-if ($opts{c} && -f $opts{c}) {
-    say "[INFO] Loading config from $opts{c}";
-    $config = YAML::LoadFile( $opts{c} );
-} elsif ($opts{'github-secret'} && $ENV{'TWITTER_TOKENS'}) {
-    say "[INFO] Loading config from env";
-    $config = YAML::Load($ENV{'TWITTER_TOKENS'});
-} else {
-    say "[INFO] No config -- dryrun.";
-}
+    my %opts;
+    GetOptionsFromArray(
+        \@args,
+        \%opts,
+        'fake-today=s',
+        'github-secret',
+        'c=s',
+        'y|yes'
+    ) or die("Error in arguments, but I'm not telling you what it is.");
 
-# 2021/08/28: https://www.cec.gov.tw/central/cms/110news/34920
-my $hashtags = "#全國性公民投票\n#台灣投票\n#TaiwanVotes";
-my $vote_date = DateTime->new(
-    year      => '2021',
-    month     => '8',
-    day       => '28',
-    hour      => '0',
-    minute    => '0',
-    second    => '0',
-    time_zone => 'Asia/Taipei',
-);
+    my $today = $opts{"fake-today"} ? DateTime_from_ymd( $opts{"fake-today"} ) : DateTime->now( time_zone => 'Asia/Taipei' )->truncate( to => 'day' );
 
-my $today = DateTime->now( time_zone => 'Asia/Taipei' )->truncate( to => 'day' );
-
-if ($opts{"fake-today"}) {
-    my @ymd = split /[\/\-]/, $opts{"fake-today"};
-    if (@ymd == 3) {
-        $today = DateTime->new(
-            year      => $ymd[0],
-            month     => $ymd[1],
-            day       => $ymd[2],
-            hour      => '0',
-            minute    => '0',
-            second    => '0',
-            time_zone => 'Asia/Taipei',
-        );
-    } else {
-        die "Unknown date format in `--fake-today`. Try: 2020/01/11"
-    }
-}
-
-my $diff_seconds = $vote_date->epoch - $today->epoch();
-my $diff_days = int $diff_seconds/86400;
-
-exit(0) if $diff_days < -1;
-
-my $msg;
-if ($diff_days > 1) {
-    $msg = sprintf('離下次投票 %s ... 還有 %s 天。', $vote_date->ymd("/"), $diff_days);
-    $msg .= "\n\n\n" . $hashtags;
-} elsif ($diff_days == 1) {
-    $msg = '投票日... 就是明天呢。' . "\n\n\n#記得去投票\n" . $hashtags;
-} elsif ($diff_days == 0) {
-    $msg = '投票日... 不就是今天嗎。' . "\n\n\n#你投票了嗎\n" . $hashtags;
-} elsif ($diff_days == -1) {
-    $msg = '投票日倒數完畢... 總算可以下班了。';
-}
-
-say encode_utf8($msg);
-
-if ($opts{y} && $config) {
-    say "Tweet for real";
-    my $twitter = Net::Twitter->new(
-        ssl => 1,
-        traits => [ 'API::RESTv1_1' ],
-        consumer_key        => $config->{consumer_key},
-        consumer_secret     => $config->{consumer_secret},
-        access_token        => $config->{access_token},
-        access_token_secret => $config->{access_token_secret},
+    # Sorted by date.
+    my @votes = (
+        # Date, Title, URL
+        ["2021/02/06", "高雄市議員黃捷罷免案", "https://www.cec.gov.tw/central/cms/110news/34965"],
+        ["2021/08/28", "全國性公民投票", "https://www.cec.gov.tw/central/cms/110news/34920"],
     );
 
-    $twitter->update($msg);
+    my $msg = build_countdown_message( $today, \@votes );
+    maybe_tweet_update(\%opts, $msg);
+
+    return 0;
 }
-else {
-    say "\n----\nNot Tweeting";
+
+exit(main(@ARGV));
+
+sub DateTime_from_ymd ($s) {
+    my @ymd = split /[\/\-]/, $s;
+
+    if (@ymd != 3) {
+        die "Unknown date format in '$s'. Try something like: 2020/01/11";
+    }
+
+    return DateTime->new(
+        year      => $ymd[0],
+        month     => $ymd[1],
+        day       => $ymd[2],
+        hour      => '0',
+        minute    => '0',
+        second    => '0',
+        time_zone => 'Asia/Taipei',
+    );
+}
+
+sub build_countdown_message ($today, $votes) {
+    my ($title, $url, $diff_days, $vote_date, $just_finished);
+    for my $vote (@$votes) {
+        $vote_date = DateTime_from_ymd( $vote->[0] );
+        $title = $vote->[1];
+        $url = $vote->[2];
+
+        $diff_days = int ($vote_date->epoch - $today->epoch())/86400;
+
+        last if $diff_days >= 0;
+
+        if ($diff_days == -1) {
+            $just_finished = 1;
+        }
+    }
+
+    my $msg;
+
+    my $hashtags .= "\#${title}\n\n#台灣投票\n#TaiwanVotes";
+    if ($diff_days > 1) {
+        if ($just_finished) {
+            $msg = "接著開始倒數下次投票日。將於 " . $vote_date->ymd("/") . " 舉辦的：\n\n    $title。\n\n詳見： $url";
+        } else {
+            $msg = "離下次投票 " . $vote_date->ymd("/") . " ... 還有 $diff_days 天。\n\n" . $hashtags;
+        }
+    } elsif ($diff_days == 1) {
+        $msg = '投票日... 就是明天呢。' . "\n\n\n#記得去投票\n" . $hashtags;
+    } elsif ($diff_days == 0) {
+        $msg = '投票日... 不就是今天嗎。' . "\n\n\n#你投票了嗎\n" . $hashtags;
+    } elsif ($diff_days == -1) {
+        $msg = '投票日倒數完畢 ! 總算可以放假了。';
+    }
+
+    return $msg;
+}
+
+sub maybe_tweet_update ($opts, $msg) {
+    unless ($msg) {
+        say "# Message is empty.";
+        return;
+    }
+
+    my $config;
+
+    if ($opts->{c} && -f $opts->{c}) {
+        say "[INFO] Loading config from $opts->{c}";
+        $config = YAML::LoadFile( $opts->{c} );
+    } elsif ($opts->{'github-secret'} && $ENV{'TWITTER_TOKENS'}) {
+        say "[INFO] Loading config from env";
+        $config = YAML::Load($ENV{'TWITTER_TOKENS'});
+    } else {
+        say "[INFO] No config.";
+    }
+
+    say "# Message";
+    say "-------8<---------";
+    say encode_utf8($msg);
+    say "------->8---------";
+
+    if ($opts->{y} && $config) {
+        say "#=> Tweet for real";
+        my $twitter = Net::Twitter->new(
+            ssl => 1,
+            traits => [ 'API::RESTv1_1' ],
+            consumer_key        => $config->{consumer_key},
+            consumer_secret     => $config->{consumer_secret},
+            access_token        => $config->{access_token},
+            access_token_secret => $config->{access_token_secret},
+        );
+
+        $twitter->update($msg);
+    } else {
+        say "#=> Not tweeting";
+    }
 }
